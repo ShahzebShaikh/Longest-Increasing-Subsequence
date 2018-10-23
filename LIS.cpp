357fe0460184a8dbab735064356e50e7e22fe182
ShahzebShaikh <40828297+ShahzebShaikh@users.noreply.github.com>
2018-10-23 16:41:34 +0530

    Update LIS.cpp

--- LIS.cpp
+++ LIS.cpp
@@ -1,3 +1,4 @@
+//this a basic program for Finding the longest increasing sunsequence in the sequence of given number.
 #include<iostream>
 #include<conio.h>
 using namespace std;
@@ -6,14 +7,17 @@ int main(){
     int a[9];
     for(int i=0;i<9;i++){
         a[i]=1;
-    }
+    }//Dynamically add data or length to the array a so that each index stores the number or length till that sequence ends,
+    // and finally wo get length of the longest sequence
     for(int i=1;i<9;i++){
             a[i]=1;
         for(int j=0;j<9;j++){
-            if(arr[i]>arr[j] && a[i]<(1+a[j]))
-                a[i]=1+a[j];
+            if(arr[i]>arr[j] && a[i]<(1+a[j]))//the condition checks that if the current element as mentioned by the ith index is greater than the previouse all elements and then checking,
+                a[i]=1+a[j];                                //whether its value is greater than the prevoius value in the a array.  
+                
         }
     }
+    //To find the maximum length fron the array a[]
     int max=a[0];
     for(int i=1;i<9;i++){
         if(max<a[i])
