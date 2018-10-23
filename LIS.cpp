05c9076afa7f1b7b97f1f946a89619f188c595f6
ShahzebShaikh <40828297+ShahzebShaikh@users.noreply.github.com>
2018-10-23 16:29:59 +0530

    Add files via upload

---
+++ LIS.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int arr[] = {10,22,9,33,21,50,41,60,80};
+    int a[9];
+    for(int i=0;i<9;i++){
+        a[i]=1;
+    }
+    for(int i=1;i<9;i++){
+            a[i]=1;
+        for(int j=0;j<9;j++){
+            if(arr[i]>arr[j] && a[i]<(1+a[j]))
+                a[i]=1+a[j];
+        }
+    }
+    int max=a[0];
+    for(int i=1;i<9;i++){
+        if(max<a[i])
+            max=a[i];
+    }
+    cout << max;
+}
